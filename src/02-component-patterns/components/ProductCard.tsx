041081c40f4e23ea605d8670025df20e07968276
LuiseUrban <aprendizdevelopment@uni2.com.co>
2026-03-10 16:18:34 -0500

    Recibir props del componente hijo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import { useProduct } from '../hooks/useProduct'; 
 import styles from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
-  return (
-    <div className={styles.productCard}>
-        <img  className={styles.productImg} src="./coffe-mug.png" alt="Taza de Café" />
-        <h1>Product card</h1>
-    </div>
-  )
+interface Props {
+    product: Product;
+}
+
+interface Product {
+    id: string;
+    title: string;
+    description: string;
+    img?: string;
+}
+
+export const ProductCard = ({ product }: Props) => {
+
+    const { counter, increaseBy } = useProduct();
+
+    return (
+        <div className={styles.productCard}>
+            <img className={styles.productImg} src={product.img || noImage} alt={product.title} />
+            <h1>{product.title}</h1>
+            <span className={styles.productDescription}>{product.description}</span>
+
+            <div className={styles.buttonsContainer}>
+                <button className={styles.buttonMinus} onClick={()=>increaseBy(-1)}>-</button>
+
+                <div className={styles.countLabel}>{ counter }</div>
+
+                <button className={styles.buttonAdd} onClick={()=>increaseBy(1)}>+</button>
+            </div>
+        </div>
+    )
 }
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,23 @@
 import { ProductCard } from "../components/ProductCard"
 
+const product = {
+    id: '1',
+    title: 'Coffe Mug',
+    description: 'This is a coffe mug',
+}
+
 export const ShoppingPage = () => {
   return (
     <>
     <div>
       <h1>ShoppingPage</h1>
       <hr />
-      <ProductCard />
+      <div style={{
+        display:'flex',
+        flexDirection: 'row',
+        flexWrap:'wrap'
+      }}></div>
+      <ProductCard product={product} />
     </div>
    
     </>
